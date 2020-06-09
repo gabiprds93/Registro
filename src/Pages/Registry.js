ed075477e7d00341b87a2302cc23e28946766a41
gabiprds93 <gabiprds.93@gmail.com>
2020-06-08 22:36:34 -0500

    add config form

--- src/Pages/Registry.js
+++ src/Pages/Registry.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Formik, Form, Field } from 'formik'
 
 import FormikInput from '../Components/ForminkInput'
-
+import { entrepreneurDataConfig } from '../config/ConfigForm'
 
 const Registry = () => {
   return(
@@ -10,7 +10,11 @@ const Registry = () => {
       <Form
         style={{ display: 'flex', flexWrap: 'wrap' }}
       >
-        <Field type="text" name="name" label="Nombre de la tienda o persona" component={FormikInput}/>
+        {entrepreneurDataConfig.map((item, index) => {
+          return (
+            <Field key={index} type={item.type} name={item.name} label={item.label} component={FormikInput}/>
+          )
+        })}
       </Form>
     </Formik>
   )
---
+++ src/config/ConfigForm.js
@@ -0,0 +1,14 @@
+export const entrepreneurDataConfig = [
+  { label: 'Nombre de la tienda o persona', type: 'text', name: 'name' },
+  { label: 'Descripción de la tienda o persona', type: 'textarea', name: 'description' },
+  { label: 'Nombre de contacto', type: 'text', name: 'contactName' },
+  { label: 'Celular de contacto', type: 'phone', name: 'cellphone' },
+  { label: 'URL Facebook', type: 'text', name: 'urlFacebook' },
+]
+
+export const accessDataConfig = [
+  { label: 'Usuario', type: 'text', name: 'user' },
+  { label: 'Contraseña', type: 'password', name: 'password' },
+  { label: 'Confirmar contraseña', type: 'password', name: 'confirmPassword' },
+  { label: 'Agregar archivos', type: 'file', name: 'addFiles' },
+]
