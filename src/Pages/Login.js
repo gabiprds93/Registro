4533410d8329e4685cdb82e0ef98910254dbfed4
gabiprds93 <gabiprds.93@gmail.com>
2020-06-12 00:07:52 -0500

    add actions for login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -4,8 +4,17 @@ import { connect } from 'react-redux'
 
 import FormikInput from '../Components/ForminkInput'
 import { loginDataConfig } from '../config/ConfigForm'
+import { fetchUserLogin } from '../redux/actions/userActions'
 
-const Login = () => {
+const Login = ({ fetchUserLogin }) => {
+  const submitForm = values => {
+    fetchUserLogin(values).then(data => {
+      if(!data.errors){
+        console.log('success')
+      }
+    })
+  }
+
   const initialValues = {
     username: '',
     clave: ''
@@ -18,7 +27,7 @@ const Login = () => {
         <h3 className="subtitle">Ingresa con tu cuenta para hacer uso de la Plataforma Digital MoliEmprendedor.</h3>
       </div>
 
-      <Formik initialValues={initialValues}>
+      <Formik initialValues={initialValues} onSubmit={submitForm}>
       {() => (
         <Form className="form">
           {loginDataConfig.map((item, index) => {
@@ -32,7 +41,7 @@ const Login = () => {
               />
             )
           })}
-          
+
           <div className="button-group">
             <button className="background-green" type="submit">Ingresar</button>
           </div>
@@ -45,6 +54,6 @@ const Login = () => {
 
 const mapStateToProps = state => state 
 
-const mapDispatchToProps = { }
+const mapDispatchToProps = {fetchUserLogin }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login)
--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -2,6 +2,10 @@ export const USER_REGISTRY_REQUEST = 'USER_REGISTRY_REQUEST'
 export const USER_REGISTRY_FAILURE = 'USER_REGISTRY_FAILURE'
 export const USER_REGISTRY_SUCCESS = 'USER_REGISTRY_SUCCESS'
 
+export const USER_LOGIN_REQUEST = 'USER_LOGIN_REQUEST'
+export const USER_LOGIN_FAILURE = 'USER_LOGIN_FAILURE'
+export const USER_LOGIN_SUCCESS = 'USER_LOGIN_SUCCESS'
+
 export const userRegistryRequest = () => ({
   type: USER_REGISTRY_REQUEST
 })
@@ -28,6 +32,36 @@ export const fetchUserRegistry = payload => {
     .then(result => dispatch(userRegistrySuccess(result)))
     .catch(error => {
       dispatch(userRegistryFailure(error.message))
+    })
+  }
+}
+
+export const userLoginRequest = () => ({
+  type: USER_LOGIN_REQUEST
+})
+
+export const userLoginSuccess = payload => ({
+  type: USER_LOGIN_SUCCESS,
+  ...payload
+})
+
+export const userLoginFailure = payload => ({
+  type: USER_LOGIN_FAILURE,
+  ...payload
+})
+
+export const fetchUserLogin = payload => {
+  return dispatch =>{
+    dispatch(userLoginRequest())
+    return fetch('https://moliemprendedor.munimolina.gob.pe/adm_login_usuario1.php', {
+      method: "POST",
+      headers: { 'Content-Type':'application/json' },
+      body: JSON.stringify(payload),
+    })
+    .then(response => response.json())
+    .then(result => dispatch(userLoginSuccess(result)))
+    .catch(error => {
+      dispatch(userLoginFailure(error.message))
     })
   }
 }
