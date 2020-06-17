64b1db28ac0aedb8b2306d1b1622ea4113caf413
gabiprds93 <gabiprds.93@gmail.com>
2020-06-16 19:11:28 -0500

    uplload multiple files

--- src/Components/InputFile.js
+++ src/Components/InputFile.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
 const InputFile = ({label, type, field}) => {
-  const [info, setInfo] = React.useState('Seleccionar archivo')
+  const [files, setFiles] = React.useState([{name: 'Seleccionar archivo'}])
+  
   const handleChange = (event) => {
-    let file = event.target.files;
-    if(file.length){
-      setInfo(file[0].name)
+    let inputFile = event.target.files
+    let arrayFiles = []
+
+    for(let i = 0; i < inputFile.length; i++){
+      arrayFiles.push({name: inputFile[i].name})
     }
+    if(inputFile.length) setFiles(arrayFiles)
   }
   
   return(
@@ -15,9 +19,11 @@ const InputFile = ({label, type, field}) => {
       <div className="file-upload">
         <label>
           <div className="form-input">
-            <span id='info'>{info}</span>
+            {files.map((item, index) => (
+              <span key={index} id='info'>{item.name}</span>
+            ))}
           </div>
-          <input id='input-file' type={type} {...field} onChange={handleChange} />
+          <input id='input-file' type={type} {...field} onChange={handleChange} multiple />
           <div className="btn background-green">Cargar</div>
         </label>
       </div>
