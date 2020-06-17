c211ac5186b10017f734c6835ea550620504be0d
gabiprds93 <gabiprds.93@gmail.com>
2020-06-16 21:25:48 -0500

    clean code

--- src/Pages/Account.js
+++ src/Pages/Account.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 const Login = ({ loginResponse, history }) => {
-  console.log(loginResponse)
+  
   React.useEffect(() => {
     if(!loginResponse){
       history.push('/login')
